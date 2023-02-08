a80b105437744fff4b1f8de862a5fcfa4bcabde0
kdfdev <124607672+kdfdev@users.noreply.github.com>
2023-02-08 13:22:14 -0600

    Update streamlit_app.py
    3rd mod on api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#makes the json pretty
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#and puts it in a table
+streamlit.dataframe(fruityvice_normalized)
